b8fcfd8016aee6811db864382c6504c4f72c0b65
daca-github <danielcornejoaaa@gmail.com>
2023-07-02 11:07:40 -0400

    Added a title and brief description to the home page

--- src/MainApp.js
+++ src/MainApp.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import './MainApp.css';
 import App from './App';
 
 function MainApp() {
   return (
     <div className="App">
       <header className="App-header">
-
+        <h2>Weather App</h2>
+        <p>A simple weather application that provides current weather information for any city in the world.</p>
+        <App />
       </header>
-      <App />
     </div>
   );
 }
