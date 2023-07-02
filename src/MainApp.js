0768037cbdbfdab844d421c13f74fc6c5d80157c
daca-github <danielcornejoaaa@gmail.com>
2023-07-02 12:36:30 -0400

    removed the header

--- src/MainApp.js
+++ src/MainApp.js
@@ -5,11 +5,7 @@ import App from './App';
 function MainApp() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h2>Weather App</h2>
-        <p>A simple weather application that provides current weather information for any city in the world.</p>
         <App />
-      </header>
     </div>
   );
 }
